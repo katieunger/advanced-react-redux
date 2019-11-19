1972a8adf2e2db5a56df971c65c4cc9a05bf969d
Katie Unger <katie.e.unger@gmail.com>
2019-11-18 23:23:34 -0500

    bonding react with redux

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
     state = { comment: '' };
@@ -10,9 +12,7 @@ class CommentBox extends Component {
     handleSubmit = (event) => {
         event.preventDefault();
 
-        // TODO
-        // Call an action creator
-        // Save the comment that got entered by the user
+        this.props.saveComment(this.state.comment);
 
         this.setState({ comment: ''});
     }
@@ -30,4 +30,4 @@ class CommentBox extends Component {
     }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
