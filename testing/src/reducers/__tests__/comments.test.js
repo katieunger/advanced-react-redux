d195b60dff0000c4a148bd6e22f50f3456864855
Katie Unger <katie.e.unger@gmail.com>
2019-11-21 15:36:21 -0500

    handling unknown types

--- testing/src/reducers/__tests__/comments.test.js
+++ testing/src/reducers/__tests__/comments.test.js
@@ -13,3 +13,9 @@ it('handles actions of type SAVE_COMMENT', () => {
 
     expect(newState).toEqual(['New Comment']);
 });
+
+it('handles action with unknown type', () => {
+    const newState = commentsReducer([], {});
+
+    expect(newState).toEqual([]);
+});
