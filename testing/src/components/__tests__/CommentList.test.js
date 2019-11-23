dc6951dbd0d9694d85f4e627905f45c69a058db3
Katie Unger <katie.e.unger@gmail.com>
2019-11-23 10:21:13 -0500

    creating comment list test; issue with getting data into redux

---
+++ testing/src/components/__tests__/CommentList.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import CommentList from 'components/CommentList';
+import Root from 'Root';
+
+let wrapped;
+
+beforeEach(() => {
+
+})
+
+it('creates one <li> per comment', () => {
+
+});
