440a54441da6aa71ac123e44c54d17d450c04ead
Katie Unger <katie.e.unger@gmail.com>
2020-09-11 11:34:47 -0400

    forwarding actions

--- testing/src/middlewares/async.js
+++ testing/src/middlewares/async.js
@@ -1,5 +1,12 @@
 // Next is a reference to the next middleware on our chain (function)
 export default ({ dispatch }) => next => action => {
-            
+    // Check to see if the action
+    // has a promise on its 'payload' property
+    // If it does, then wait for it to resolve
+    // If it doesn't, then send the action on to the
+    // next middleware  
+    if (!action.payload || !action.payload.then) {
+        return next(action);
+    }
 }
     
